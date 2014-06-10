be4c42628edbb5eb183b05caf3c39906e15db2a3
Mike Bannister <notimpossiblemike@gmail.com>
2014-06-10 12:06:24 -0700

    Rebuilt

--- dist/api.js
+++ dist/api.js
@@ -13,76 +13,80 @@ module.exports = {
 },{}],2:[function(_dereq_,module,exports){
 module.exports = {
 
-	startListening: function(){
-		this.sendMessage('startListening');
-	},
+  startListening: function(){
+    this.sendMessage('startListening');
+  },
 
-	setHeight: function(px){
-		this.sendMessage('setHeight', { pixels: px });
-	},
+  setHeight: function(px){
+    this.sendMessage('setHeight', { pixels: px });
+  },
 
-	setAttribute: function(name, value){
-		var attr = {};
-		attr[name] = value;
-		this.setAttributes(attr);
-	},
+  setAttribute: function(name, value){
+    var attr = {};
+    attr[name] = value;
+    this.setAttributes(attr);
+  },
 
-	setAttributes: function(attrs) {
-		this.sendMessage('setAttributes', attrs);
-	},
+  setAttributes: function(attrs) {
+    this.sendMessage('setAttributes', attrs);
+  },
 
-	setLearnerAttribute: function(name, value){
-		var attr = {};
-		attr[name] = value;
-		this.setLearnerState(attr);
-	},
+  setLearnerAttribute: function(name, value){
+    var attr = {};
+    attr[name] = value;
+    this.setLearnerState(attr);
+  },
 
-	setLearnerAttributes: function(attrs) {
-		this.sendMessage('setLearnerState', attrs);
-	},
+  setLearnerAttributes: function(attrs) {
+    this.sendMessage('setLearnerState', attrs);
+  },
 
-	setLearnerState: function(attrs) {
-		this.sendMessage('setLearnerState', attrs);
-	},
+  setLearnerState: function(attrs) {
+    this.sendMessage('setLearnerState', attrs);
+  },
 
-	setPropertySheetAttributes: function(attrs){
-		this.sendMessage('setPropertySheetAttributes', attrs);
-	},
+  setPropertySheetAttributes: function(attrs){
+    this.sendMessage('setPropertySheetAttributes', attrs);
+  },
 
-	setEmpty: function(empty){
-		this.sendMessage('setEmpty', { empty: empty });
-	},
+  setEmpty: function(empty){
+    this.sendMessage('setEmpty', { empty: empty });
+  },
 
-	track: function(name, data){
-		var message = {
-			event: name,
-			data: data
-		};
-		this.sendMessage('track', message);
-	},
+  track: function(name, _data){
+    var data = { '@type': name };
+    Object.keys(_data).forEach(function(key){
+      data[key] = _data[key];
+    });
+    this.sendMessage('track', data);
+  },
 
-	error: function(data){
-		this.sendMessage('error', data);
-	},
+  error: function(data){
+    this.sendMessage('error', data);
+  },
 
-	// controversial
-	changeBlocking: function(){
-		this.sendMessage('changeBlocking');
-	},
+  // controversial
+  changeBlocking: function(){
+    this.sendMessage('changeBlocking');
+  },
 
-	requestAsset: function(data, callback){
-		if(!data.attribute) {
-			data.attribute = '__asset__';
-		}
-		// TODO: remove this after assets are communicated from the player
-		// in a dedicated event
-		this._assetAttributes[data.attribute] = true;
+  requestAsset: function(data, callback){
+    if(!data.attribute) {
+      data.attribute = '__asset__';
+    }
+    // TODO: remove this after assets are communicated from the player
+    // in a dedicated event
+    this._assetAttributes[data.attribute] = true;
 
-		if(callback) {
-			this._assetCallbacks[data.attribute] = callback;
-		}
-		this.sendMessage('requestAsset', data);
-	}
+    if(callback) {
+      this._assetCallbacks[data.attribute] = callback;
+    }
+    this.sendMessage('requestAsset', data);
+  },
+
+  assetUrl: function(id){
+    return this.assetUrlTemplate.replace(/<%= id %>/, id);
+  }
 };
 
 },{}],3:[function(_dereq_,module,exports){
@@ -152,10 +156,6 @@ PlayerAPI.prototype.handleMessage = function(evt) {
       this.assetUrlTemplate = message.data.assetUrlTemplate;
     }
   }
-};
-
-PlayerAPI.prototype.assetUrl = function(id){
-  return this.assetUrlTemplate.replace('<%= id %>', id);
 };
 
 // TODO: move implementation to the player
